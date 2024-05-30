0a934e8c01fccf7c5c5ddf106c5dffecebc1cdf3
dijunkun <junkun.di@hotmail.com>
2024-05-30 16:33:50 +0800

    Fix LNK1561 error on Winodws

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,3 +1,11 @@
+#ifdef _WIN32
+#ifdef REMOTE_DESK_DEBUG
+#pragma comment(linker, "/subsystem:\"console\"")
+#else
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#endif
+#endif
+
 #include "connection.h"
 #include "log.h"
 #include "main_window.h"
