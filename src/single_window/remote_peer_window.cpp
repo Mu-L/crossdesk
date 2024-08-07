ec236563349edb8a1694c77e0e3cda6ff18759d4
dijunkun <junkun.di@hotmail.com>
2024-08-07 09:55:59 +0800

    [fix] fix client id error when connect to itself

--- src/single_window/remote_peer_window.cpp
+++ src/single_window/remote_peer_window.cpp
@@ -57,11 +57,13 @@ int Render::RemoteWindow() {
       int ret = -1;
       if (signal_connected_) {
         if (!connection_established_) {
-          if (remote_id_ == local_id_ && !peer_reserved_) {
+          if (0 == strcmp(remote_id_, client_id_) && !peer_reserved_) {
             peer_reserved_ = CreatePeer(&params_);
             if (peer_reserved_) {
               LOG_INFO("Create peer[reserved] instance successful");
-              Init(peer_reserved_, "");
+              std::string client_id = "C-";
+              client_id += client_id_;
+              Init(peer_reserved_, client_id.c_str());
               LOG_INFO("Peer[reserved] init finish");
             } else {
               LOG_INFO("Create peer[reserved] instance failed");
