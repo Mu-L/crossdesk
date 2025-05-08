e2a469ed65145cd646e72ef81c8cf0b7d8f043b6
dijunkun <junkun.di@hotmail.com>
2025-05-08 11:33:04 +0800

    [feat] remove dependence on ffmpeg for MacOSx

--- xmake.lua
+++ xmake.lua
@@ -33,13 +33,11 @@ elseif is_os("linux") then
     add_links("SDL2", "cuda", "nvidia-encode", "nvcuvid", "X11", "Xtst")
     add_cxflags("-Wno-unused-variable")   
 elseif is_os("macosx") then
-    add_requires("ffmpeg 5.1.2", {system = false})
-    add_requires("libxcb", {system = false})
-    add_packages("libxcb")
     add_links("SDL2", "SDL2main")
     add_ldflags("-Wl,-ld_classic")
     add_cxflags("-Wno-unused-variable")
-    add_frameworks("OpenGL", "IOSurface", "ScreenCaptureKit")
+    add_frameworks("OpenGL", "IOSurface", "ScreenCaptureKit", "AVFoundation", 
+        "CoreMedia", "CoreVideo")
 end
 
 add_packages("spdlog", "imgui")
@@ -67,14 +65,11 @@ target("screen_capturer")
         add_files("src/screen_capturer/windows/*.cpp")
         add_includedirs("src/screen_capturer/windows", {public = true})
     elseif is_os("macosx") then
-        add_packages("ffmpeg")
-        add_files("src/screen_capturer/macosx/avfoundation/*.cpp",
-        "src/screen_capturer/macosx/screen_capturer_kit/*.cpp",
-        "src/screen_capturer/macosx/screen_capturer_kit/*.mm")
-        add_includedirs("src/screen_capturer/macosx/avfoundation",
-        "src/screen_capturer/macosx/screen_capturer_kit", {public = true})
+        add_files("src/screen_capturer/macosx/*.cpp",
+        "src/screen_capturer/macosx/*.mm")
+        add_includedirs("src/screen_capturer/macosx", {public = true})
     elseif is_os("linux") then
-        add_packages("libyuv", "ffmpeg")
+        add_packages("libyuv")
         add_files("src/screen_capturer/linux/*.cpp")
         add_includedirs("src/screen_capturer/linux", {public = true})
     end
@@ -129,12 +124,8 @@ target("localization")
 target("single_window")
     set_kind("object")
     add_packages("libyuv", "openssl3")
-    add_deps("rd_log", "common", "localization", "config_center", "projectx", "screen_capturer", "speaker_capturer", "device_controller")
-    if is_os("macosx") then
-        add_packages("ffmpeg")
-    elseif is_os("linux") then
-        add_packages("ffmpeg")
-    end
+    add_deps("rd_log", "common", "localization", "config_center", "projectx", 
+        "screen_capturer", "speaker_capturer", "device_controller")
     add_files("src/single_window/*.cpp")
     add_includedirs("src/single_window", {public = true})
     add_includedirs("fonts", {public = true})
@@ -145,77 +136,8 @@ target("remote_desk")
     if is_os("windows") then
         add_files("icon/app.rc")
     elseif is_os("macosx") then
-        add_packages("ffmpeg")
         -- add_rules("xcode.application")
         -- add_files("Info.plist")
     elseif is_os("linux") then
-        add_packages("ffmpeg")
     end
     add_files("src/gui/main.cpp")
-
--- target("miniaudio_capture")
---     set_kind("binary")
---     add_packages("miniaudio")
---     if is_os("windows") then
---         add_files("test/audio_capture/miniaudio.cpp")
---     end
-
--- target("screen_capturer")
---     set_kind("binary")
---     add_packages("sdl2", "imgui",  "ffmpeg", "openh264")
---     add_files("test/screen_capturer/linux_capture.cpp")
---     add_ldflags("-lavformat", "-lavdevice", "-lavfilter", "-lavcodec",
---     "-lswscale", "-lavutil", "-lswresample",
---     "-lasound", "-lxcb-shape", "-lxcb-xfixes", "-lsndio", "-lxcb", 
---     "-lxcb-shm", "-lXext", "-lX11", "-lXv", "-lpthread", "-lSDL2", "-lopenh264",
---     "-ldl", {force = true})
-
--- target("screen_capturer")
---     set_kind("binary")
---     add_packages("sdl2", "imgui",  "ffmpeg", "openh264")
---     add_files("test/screen_capturer/mac_capture.cpp")
---     add_ldflags("-lavformat", "-lavdevice", "-lavfilter", "-lavcodec",
---     "-lswscale", "-lavutil", "-lswresample",
---     "-lasound", "-lxcb-shape", "-lxcb-xfixes", "-lsndio", "-lxcb", 
---     "-lxcb-shm", "-lXext", "-lX11", "-lXv", "-lpthread", "-lSDL2", "-lopenh264",
---     "-ldl", {force = true})
-
--- target("audio_capture")
---     set_kind("binary")
---     add_packages("ffmpeg")
---     add_files("test/audio_capture/sdl2_audio_capture.cpp")
---     add_includedirs("test/audio_capture")
---     add_ldflags("-lavformat", "-lavdevice", "-lavfilter", "-lavcodec",
---     "-lswscale", "-lavutil", "-lswresample",
---     "-lasound", "-lxcb-shape", "-lxcb-xfixes", "-lsndio", "-lxcb", 
---     "-lxcb-shm", "-lXext", "-lX11", "-lXv", "-lpthread", "-lSDL2", "-lopenh264",
---     "-ldl", {force = true})
---     add_links("Shlwapi", "Strmiids", "Vfw32", "Secur32", "Mfuuid")
-
--- target("play_audio")
---     set_kind("binary")
---     add_packages("ffmpeg")
---     add_files("test/audio_capture/play_loopback.cpp")
---     add_includedirs("test/audio_capture")
---     add_ldflags("-lavformat", "-lavdevice", "-lavfilter", "-lavcodec",
---     "-lswscale", "-lavutil", "-lswresample",
---     "-lasound", "-lxcb-shape", "-lxcb-xfixes", "-lsndio", "-lxcb", 
---     "-lxcb-shm", "-lXext", "-lX11", "-lXv", "-lpthread", "-lSDL2", "-lopenh264",
---     "-ldl", {force = true})
---     add_links("Shlwapi", "Strmiids", "Vfw32", "Secur32", "Mfuuid")
-
--- target("audio_capture")
---     set_kind("binary")
---     add_packages("libopus")
---     add_files("test/audio_capture/sdl2_audio_capture.cpp")
---     add_includedirs("test/audio_capture")
---     add_ldflags("-lavformat", "-lavdevice", "-lavfilter", "-lavcodec",
---     "-lswscale", "-lavutil", "-lswresample",
---     "-lasound", "-lxcb-shape", "-lxcb-xfixes", "-lsndio", "-lxcb", 
---     "-lxcb-shm", "-lXext", "-lX11", "-lXv", "-lpthread", "-lSDL2", "-lopenh264",
---     "-ldl", {force = true})
-
--- target("mouse_control")
---     set_kind("binary")
---     add_files("test/linux_mouse_control/mouse_control.cpp")
---     add_includedirs("test/linux_mouse_control")
